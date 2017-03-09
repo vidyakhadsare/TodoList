3ff872c903e9781be746f28f19563a979af6e31b
Vidya Khadsare <vidya.khadsare@gmail.com>
2017-03-09 11:31:29 -0800

    Add todo app component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render : function(){
+      return(
+        <div>TodoApp</div>
+      );
+  }
+});
+
+module.exports = TodoApp;
