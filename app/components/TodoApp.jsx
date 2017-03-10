8c83cfa2c5881f1a3817fc0add792b67a3762af3
Vidya Khadsare <vidya.khadsare@gmail.com>
2017-03-10 08:46:42 -0800

    to-do list with static data

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,27 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
 
+    return{
+      todos: [{
+        id: 1,
+        text: 'Walk the dog'
+      },
+      {
+        id: 2,
+        text: 'Clean the yard'
+      }]
+    };
+  },
   render : function(){
+      var {todos} = this.state;
       return(
-        <div>TodoApp</div>
+        <div>
+        <TodoList todos={todos}/>
+        </div>
       );
   }
 });
-
 module.exports = TodoApp;
